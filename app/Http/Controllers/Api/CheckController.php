1d758b7f8f57ce3ee0794b1adb51bde804865b66
Jeyhun Rashidov <ceyhun@unitedskills.az>
2021-08-18 16:35:42 +0400

    Asd

--- app/Http/Controllers/Api/CheckController.php
+++ app/Http/Controllers/Api/CheckController.php
@@ -4,19 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use DB;
 
 class CheckController extends Controller
 {
     public function contentCheck()
     {
-        return DB::select('SHOW TABLES');
-        
-        if(DB::connection()->getDatabaseName())
-        {
-            return "conncted sucessfully to database ".DB::connection()->getDatabaseName();
-        }
-
         $url = "python C:/Users/User/Desktop/www/abyss-hub/public/python/contentChecker/check.py 2>&1";
         $data = "C:/Users/User/Desktop/www/abyss-hub/public/python/contentChecker/tests/file.py";
         return shell_exec( $url . escapeshellarg($data) );
