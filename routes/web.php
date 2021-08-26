c599ff997b9a042ba83f9ee8cc15d9184d9ed522
Jeyhun <creshidov23@gmail.com>
2021-08-26 11:52:39 +0400

    Asd

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes(['verify' => true]);
 
-Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
