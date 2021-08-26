fee6ecee85e20dfa230bd56c346062474567c261
Jeyhun <creshidov23@gmail.com>
2021-08-26 17:36:17 +0400

    Asd

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -30,7 +30,6 @@ class AuthController extends Controller
 
     public function register(RegisterRequest $request)
     {
-        return $this->sendError('The given data was invalid', null, 401);
         try {
             $user = new User([
                 'email' => $request->email,
