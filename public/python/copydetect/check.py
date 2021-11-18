750d75100fb7d0e1564785ef2d6a1b56764e23b1
Jeyhun <creshidov23@gmail.com>
2021-11-18 10:47:37 +0400

    sad

--- public/python/copydetect/check.py
+++ public/python/copydetect/check.py
@@ -1,10 +1,11 @@
-import os
-import sys
-from plagiarismdetect import CopyDetector
+print("Hello world")
+# import os
+# import sys
+# from plagiarismdetect import CopyDetector
 
-directory = os.path.normpath(os.path.join(__file__,'../../../../'))
+# directory = os.path.normpath(os.path.join(__file__,'../../../../'))
 
-detector = CopyDetector(test_dirs=[f"{directory}/storage/app/products/live"], display_t=0.1)
-detector.add_file(f"{directory}/storage/app/products/temporary/{sys.argv[1]}")
-detector.run()
-detector.generate_html_report()
+# detector = CopyDetector(test_dirs=[f"{directory}/storage/app/products/live"], display_t=0.1)
+# detector.add_file(f"{directory}/storage/app/products/temporary/{sys.argv[1]}")
+# detector.run()
+# detector.generate_html_report()
