835160fd70ffdd4b93c8d17c4dced969f3a095ad
Jeyhun <creshidov23@gmail.com>
2021-11-18 13:12:20 +0400

    Asd

--- app/Http/Controllers/Api/Store/ProductController.php
+++ app/Http/Controllers/Api/Store/ProductController.php
@@ -48,9 +48,9 @@ class ProductController extends Controller
         $file = md5(time()).'.py';
         Storage::disk('products')->put( 'temporary/'.$file, $request->source_code);
 
-        $url = "python3 /var/www/abysshub/public/python/copydetect/check.py 2>&1";
+        $url = "python3 /var/www/abysshub/public/python/copydetect/check.py ";
         // $url = "python C:/Users/User/Desktop/www/abyss-hub/public/python/copydetect/check.py 2>&1";
-        $result = shell_exec( $url );
+        $result = shell_exec( $url . $file);
         return $result;
 
         $result = true;
--- public/python/copydetect/check.py
+++ public/python/copydetect/check.py
@@ -1,14 +1,10 @@
 import os
 import sys
-print(os.system("python3 -m site --user-site"))
-
 sys.path.append("/home/abysshub_023/.local/lib/python3.8/site-packages")
-
 from plagiarismdetect import CopyDetector
 
 directory = os.path.normpath(os.path.join(__file__,'../../../../'))
-# {sys.argv[1]}
 detector = CopyDetector(test_dirs=[f"{directory}/storage/app/products/live"], display_t=0.1)
-detector.add_file(f"{directory}/storage/app/products/temporary/0236260da63c1fa93f94e98035f8482a.py")
+detector.add_file(f"{directory}/storage/app/products/temporary/{sys.argv[1]}")
 detector.run()
 detector.generate_html_report()
