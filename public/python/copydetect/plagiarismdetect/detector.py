a504863f1a7de2ef4294ab87169c7e1104ac44e4
Jeyhun <creshidov23@gmail.com>
2021-11-18 12:20:29 +0400

    ads

--- public/python/copydetect/plagiarismdetect/detector.py
+++ public/python/copydetect/plagiarismdetect/detector.py
@@ -4,6 +4,8 @@ reference files (files that might have been plagairised from).
 """
 
 from pathlib import Path
+import sys
+sys.path.append("/home/abysshub_023/.local/lib/python3.8/site-packages/numpy")
 import numpy as np
 import logging
 from .utils import (filter_code, highlight_overlap, get_copied_slices,
