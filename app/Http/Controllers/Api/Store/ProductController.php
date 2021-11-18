6e5d7aaf0bb6a8d51bb821b0fef417d8e8047d32
Jeyhun <creshidov23@gmail.com>
2021-11-18 10:51:36 +0400

    ads

--- app/Http/Controllers/Api/Store/ProductController.php
+++ app/Http/Controllers/Api/Store/ProductController.php
@@ -49,7 +49,9 @@ class ProductController extends Controller
         Storage::disk('products')->put( 'temporary/'.$file, $request->source_code);
 
         $url = "python /var/www/abysshub/public/python/copydetect/check.py 2>&1";
+        echo $url;
         $result = shell_exec( $url . $file );
+        echo $file;
         return $result;
 
         $result = true;
