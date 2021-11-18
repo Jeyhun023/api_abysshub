daa376d1fc015d012b1939345c16eaa2220dfddd
Jeyhun <creshidov23@gmail.com>
2021-11-18 12:53:45 +0400

    sad

--- public/python/copydetect/check.py
+++ public/python/copydetect/check.py
@@ -4,8 +4,6 @@ from importlib import import_module
 
 plagiarismdetect = import_module("/home/abysshub_023/.local/lib/python3.8/site-packages/plagiarismdetect")
 
-sys.path.append("/home/abysshub_023/.local/lib/python3.8/site-packages")
-
 directory = os.path.normpath(os.path.join(__file__,'../../../../'))
 # {sys.argv[1]}
 detector = plagiarismdetect.CopyDetector(test_dirs=[f"{directory}/storage/app/products/live"], display_t=0.1)
