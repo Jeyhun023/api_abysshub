e4a265d70c7e4e50ed0cfbd073e95bfb7aabe3ee
Jeyhun <creshidov23@gmail.com>
2021-11-18 12:51:56 +0400

    asd

--- public/python/copydetect/check.py
+++ public/python/copydetect/check.py
@@ -1,12 +1,13 @@
 import os
 import sys
-from plagiarismdetect import CopyDetector
+
+plagiarismdetect = import_module("/home/abysshub_023/.local/lib/python3.8/site-packages/plagiarismdetect")
 
 sys.path.append("/home/abysshub_023/.local/lib/python3.8/site-packages")
 
 directory = os.path.normpath(os.path.join(__file__,'../../../../'))
 # {sys.argv[1]}
-detector = CopyDetector(test_dirs=[f"{directory}/storage/app/products/live"], display_t=0.1)
+detector = plagiarismdetect.CopyDetector(test_dirs=[f"{directory}/storage/app/products/live"], display_t=0.1)
 detector.add_file(f"{directory}/storage/app/products/temporary/0236260da63c1fa93f94e98035f8482a.py")
 detector.run()
 detector.generate_html_report()
