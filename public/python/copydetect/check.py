3fe38459408894d2c33591558f952b3480123cc8
Jeyhun <creshidov23@gmail.com>
2021-11-25 18:36:43 +0400

    ASd

--- public/python/copydetect/check.py
+++ public/python/copydetect/check.py
@@ -3,9 +3,9 @@ import os
 sys.path.append("/home/abysshub_023/.local/lib/python3.10/site-packages")
 print("Asfs")
 from plagiarismdetect import CopyDetector
-# directory = os.path.normpath(os.path.join(__file__,'../../../../'))
-# detector = CopyDetector(test_dirs=[f"{directory}/storage/app/products/live"], display_t=0.1)
-# detector.add_file(f"{directory}/storage/app/products/temporary/{sys.argv[1]}", f"{sys.argv[2]}")
+directory = os.path.normpath(os.path.join(__file__,'../../../../'))
+detector = CopyDetector(test_dirs=[f"{directory}/storage/app/products/live"], display_t=0.1)
+detector.add_file(f"{directory}/storage/app/products/temporary/{sys.argv[1]}", f"{sys.argv[2]}")
 
-# detector.run()
-# detector.generate_html_report()
+detector.run()
+detector.generate_html_report()
