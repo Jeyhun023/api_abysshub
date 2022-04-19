fe9db925f789afb487605905e48fb5f7a9ad78d4
Jeyhun Rashidov <creshidov23@gmail.com>
2022-04-19 14:51:56 +0400

    image cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,4 @@
 <?php
-// TODOLIST Delete 3 line above
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *'); 
 
 /*
 |--------------------------------------------------------------------------
