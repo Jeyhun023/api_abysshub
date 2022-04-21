2e91bbab65e2f601ed47184394ce0c423760f82c
Jeyhun Rashidov <creshidov23@gmail.com>
2022-04-21 16:42:37 +0400

    github

--- app/Http/Controllers/Api/Auth/SocialiteController.php
+++ app/Http/Controllers/Api/Auth/SocialiteController.php
@@ -33,45 +33,47 @@ class SocialiteController extends Controller
         if(!in_array($social, User::SOCIAL_TYPES)){
             abort(404);
         }
+        
+        try {
+            $socialUser = Socialite::driver($social)->stateless()->user();
+        } catch (\Throwable $errors) {
+            return $this->errorResponse(["failed" => [trans('messages.failed')] ]);
+        }
+           
         $user = User::where('email', $socialUser->getEmail())
             ->where('socialite_type', '!=', array_search($social, User::SOCIAL_TYPES))
             ->exists();
         if ($user) {
             return $this->errorResponse(["failed" => [trans('auth.email.exist')]]);
         }
-        try {
-            $socialUser = Socialite::driver($social)->stateless()->user();
-           
-            $user = User::updateOrCreate([
-                'socialite_id' => $socialUser->getId(),
-                'socialite_type' => array_search($social, User::SOCIAL_TYPES)
-            ], [
-                'fullname' => $socialUser->getName(),
-                //TODOLIST Check if username exist or not
-                'username' => strtolower(str_replace(' ', '', $socialUser->getName())),
-                'email' => $socialUser->getEmail(),
-                'password' => Hash::make(Str::random(40).'@'.$socialUser->getId()),
-                'socialite_token' => $socialUser->token,
-                'socialite_refresh_token' => $socialUser->refreshToken,
-                'email_verified_at' => Carbon::now()
-            ]);
 
-            $shop = new Shop();
-            $shop->user_id = $user->id;
-            $shop->name = $user->fullname."'s shop";
-            $shop->slug = Str::slug($shop->name);
-            $shop->save();
+        $user = User::updateOrCreate([
+            'socialite_id' => $socialUser->getId(),
+            'socialite_type' => array_search($social, User::SOCIAL_TYPES)
+        ], [
+            'fullname' => $socialUser->getName(),
+            //TODOLIST Check if username exist or not
+            'username' => strtolower(str_replace(' ', '', $socialUser->getName())),
+            'email' => $socialUser->getEmail(),
+            'password' => Hash::make(Str::random(40).'@'.$socialUser->getId()),
+            'socialite_token' => $socialUser->token,
+            'socialite_refresh_token' => $socialUser->refreshToken,
+            'email_verified_at' => Carbon::now()
+        ]);
 
-            $tokenResult = $user->createToken($this->pac);
+        $shop = new Shop();
+        $shop->user_id = $user->id;
+        $shop->name = $user->fullname."'s shop";
+        $shop->slug = Str::slug($shop->name);
+        $shop->save();
 
-            $success['user'] = new UserResource($user);
-            $success['access_token'] = $tokenResult->accessToken;
-            $success['token_type'] = 'Bearer';
-            $success['expires_at'] = Carbon::parse($tokenResult->token?->expires_at)->toDateTimeString();
-            
-            return $this->successResponse($success, trans('messages.register_success'));
-        } catch (\Throwable $errors) {
-            return $this->errorResponse(["failed" => [trans('messages.failed')] ]);
-        }
+        $tokenResult = $user->createToken($this->pac);
+
+        $success['user'] = new UserResource($user);
+        $success['access_token'] = $tokenResult->accessToken;
+        $success['token_type'] = 'Bearer';
+        $success['expires_at'] = Carbon::parse($tokenResult->token?->expires_at)->toDateTimeString();
+        
+        return $this->successResponse($success, trans('messages.register_success'));
     }
 }
